d409e049572e981196940e6ab65a21e6b104072f
Aloïs Micard <lunamicard@gmail.com>
2019-08-03 08:38:03 +0200

    clone.sh: now pull from master branch

--- clone.sh
+++ clone.sh
@@ -6,6 +6,6 @@ while read -r project; do
   if test ! -d "../${project}"; then
     echo "Cloning new project: ${project}"
     PROJECT_PATH="${REPO_URL}${project}.git"
-    git clone "${PROJECT_PATH}" "../${project}" --branch develop
+    git clone "${PROJECT_PATH}" "../${project}"
   fi
 done <"projects.txt"
