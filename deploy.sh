c388a6953b40c47293f2cbd36914a609b0ecb8d8
Aloïs Micard <lunamicard@gmail.com>
2019-09-18 16:34:22 +0200

    Store number of instance per process as variable

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+CRAWLER_INSTANCE=1
+SCHEDULER_INSTANCE=1
+PERSISTER_INSTANCE=1
+
 # first of all build all projects
+# todo change the way of collecting projects
 while read -r project; do
   if test ! -d "../${project}"; then
     echo "Project ${project} does not exist locally!"
@@ -11,4 +16,6 @@ while read -r project; do
 done <"projects.txt"
 
 # then bring up using docker-compose
-docker-compose up --build --scale crawler=1 --scale scheduler=1 --scale persister=1
+docker-compose up --build --scale crawler=$CRAWLER_INSTANCE \
+                          --scale scheduler=$SCHEDULER_INSTANCE \
+                          --scale persister=$PERSISTER_INSTANCE
